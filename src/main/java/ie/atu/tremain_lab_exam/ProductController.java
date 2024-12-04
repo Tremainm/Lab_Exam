fa1ac247f96e75b5762c91c1cfffaad400accaab
Tremain <g00367973@atu.ie>
2024-12-04 10:24:51 +0000

    added business logic to service class, error when attempting to run

--- src/main/java/ie/atu/tremain_lab_exam/ProductController.java
+++ src/main/java/ie/atu/tremain_lab_exam/ProductController.java
@@ -1,7 +1,10 @@
 package ie.atu.tremain_lab_exam;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/product")
@@ -10,5 +13,35 @@ public class ProductController {
 
     public ProductController(ProductService myService) {
         this.myService = myService;
+    }
+
+    private List<Product> list = new ArrayList<>();
+
+    @GetMapping("")
+    public List<Product> getProduct(String productCode)
+    {
+        list = myService.getProducts(productCode);
+        return list;
+    }
+
+    @PostMapping("")
+    public List<Product> addProduct(@Valid @RequestBody Product product)
+    {
+        list = myService.addProducts(product);
+        return list;
+    }
+
+    @PutMapping("/{productCode}")
+    public List<Product> updateProduct(@Valid Product product, String productCode)
+    {
+        list = myService.updateProducts(product, productCode);
+        return list;
+    }
+
+    @DeleteMapping("/{productCode}")
+    public List<Product> deleteProduct(@Valid String productCode)
+    {
+        list = myService.deleteProducts(productCode);
+        return list;
     }
 }

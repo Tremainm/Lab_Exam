2d3ab9503101f9694fe05c35291ff3c61c9c45b9
Tremain <g00367973@atu.ie>
2024-12-04 09:05:53 +0000

    pushed master to github

---
+++ src/main/java/ie/atu/tremain_lab_exam/TremainLabExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.tremain_lab_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TremainLabExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TremainLabExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/tremain_lab_exam/TremainLabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.tremain_lab_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TremainLabExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
